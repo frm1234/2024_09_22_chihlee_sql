6045609e6afda8ba3a3857ccacdd1cb5b84261ca
9*9 <n.b.3351@gmail.com>
2024-09-29 15:31:06 +0800

    更新表格建立

--- SQL/sql練習建表格.sql
+++ SQL/sql練習建表格.sql
@@ -7,3 +7,13 @@ create table if not exists hospital (
 	聯絡人 varchar(7),
 	地址 varchar(50)	
 );
+
+CREATE TABLE IF NOT EXISTS hospital1 (
+    A serial primary key,
+    B VARCHAR(5) NOT NULL,
+	C VARCHAR(30) NOT NULL UNIQUE,
+	d VARCHAR(15),
+	e VARCHAR(7),
+	f VARCHAR(7),
+	g VARCHAR(50)
+);
