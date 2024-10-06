f27bd4d8d3e1bb873ec01a80c3a199a30ba9ba14
darom <n.b.3351@gmail.com>
2024-10-06 14:31:20 +0800

    new

--- SQL/sql 練習抓取資料(車站).sql
+++ SQL/sql 練習抓取資料(車站).sql
@@ -15,5 +15,22 @@ SELECT code as 代碼, name as 車站名稱
        ,e_name as 英文名稱
 FROM public.station
 where code like '100%'
-
 ORDER BY id ASC ;
+
+SELECT code as 代碼, name as 車站名稱
+       ,e_name as 英文名稱
+FROM public.station
+where code
+ORDER BY id ASC ;
+
+select * from station;
+
+select *
+from station
+where  id < 20 and id > 10
+ORDER BY id desc ;
+
+
+select *
+from station
+ORDER BY e_name ;
