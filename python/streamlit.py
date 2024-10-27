1d2818a1c48b960af40276bc1bd01eb286e91ca8
99 <n.b.3351@gmail.com>
2024-10-27 14:47:59 +0800

    new

--- python/streamlit.py
+++ python/streamlit.py
@@ -6,7 +6,6 @@ conn = psycopg2.connect(
     password="C3Cwqsvu4r5TnMUOqa3CqDY5f03efO0T",
     host="dpg-csabsbi3esus739rc2m0-a.singapore-postgres.render.com",  # 通常是 '127.0.0.1' 或 'localhost'
     port="5432")
-
 cur = conn.cursor()
 cur.execute("SELECT * FROM actor;")
 rows = cur.fetchall()
